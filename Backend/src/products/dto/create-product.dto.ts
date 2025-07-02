c48134c6e37a52db0ba075c8ce54008a43f9af60
johnnyw0 <johnnyawatanabe@gmail.com>
2025-07-02 15:41:35 -0300

    (quase) finalizando ver inicial do backend

--- Backend/src/products/dto/create-product.dto.ts
+++ Backend/src/products/dto/create-product.dto.ts
@@ -1,38 +1,38 @@
-import { IsBoolean, IsString, IsNotEmpty, IsNumber, IsOptional } from "class-validator";
+import { IsBoolean, IsString, IsNotEmpty, IsNumber} from "class-validator";
 
 
 export class CreateProductDto {
 
-    @IsNotEmpty({ message: 'O modelo é obrigatório.' })
     @IsString({ message: 'O modelo deve ser uma string.' })
+    @IsNotEmpty({ message: 'O modelo é obrigatório.' })
     model: string;
 
-    @IsNotEmpty({ message: 'A marca é obrigatória.' })
     @IsString({ message: 'A marca deve ser uma string.' })
+    @IsNotEmpty({ message: 'A marca é obrigatória.' })
     brand: string;
 
-    @IsNotEmpty({ message: 'O tipo é obrigatório.' })
     @IsString({ message: 'O tipo deve ser uma string.' })
+    @IsNotEmpty({ message: 'O tipo é obrigatório.' })
     type: string;
 
-    @IsOptional() 
     @IsString({ message: 'O comprimento focal deve ser uma string.' })
-    focalLength?: string; 
+    @IsNotEmpty({ message: 'O comprimento focal é obrigatório.' })
+    focalLength: string; 
 
-    @IsOptional()
     @IsString({ message: 'A abertura máxima deve ser uma string.' })
-    maxAperture?: string;
+    @IsNotEmpty({ message: 'A abertura máxima é obrigatória.' })
+    maxAperture: string;
 
-    @IsOptional()
-    @IsString({ message: 'O mount deve ser uma string.' })
-    mount?: string;
+    @IsString({ message: 'Mount deve ser uma string.' })
+    @IsNotEmpty({ message: 'Mount é obrigatório.' })
+    mount: string;
 
-    @IsNotEmpty({ message: 'O peso é obrigatório.' })
     @IsNumber({}, { message: 'O peso deve ser um número.' })
+    @IsNotEmpty({ message: 'O peso é obrigatório.' })
     weight: number; 
 
-    @IsOptional()
     @IsBoolean({ message: 'hasStabilization deve ser um booleano.' })
+    @IsNotEmpty({ message: 'hasStabilization é obrigatório.' })
     hasStabilization?: boolean;
 
 
--- Backend/src/products/products.service.ts
+++ Backend/src/products/products.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './product.model';
 import { v4 } from 'uuid' 
 
 
+
 @Injectable()
 export class ProductsService {
+
+  private products: Product[] = []
+
   create(createProductDto: CreateProductDto): Product {
     const newProduct: Product = {
       id: v4(),
@@ -14,22 +18,41 @@ export class ProductsService {
       active: true,
       hasStabilization: createProductDto.hasStabilization ?? false, 
     };
+    this.products.push(newProduct);
     return newProduct;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  findAll(): Product[] {
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    const product = this.products.find(p => p.id === id && p.active);
+    if (!product) {
+      throw new NotFoundException(`Produto com id: "${id}" não encontrado ou está inativo.`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto): Product {
+
+  const productIndex = this.products.findIndex(p => p.id === id && p.active);
+
+  if (productIndex === -1) {
+    throw new NotFoundException(`Produto com ID "${id}" não encontrado ou inativo.`);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  this.products[productIndex] = {
+    ...this.products[productIndex],
+    ...updateProductDto,
+  };
+  return this.products[productIndex];
+}
+
+  remove(id: string) {
+    const product = this.findOne(id);
+    product.active = false;
+    return product;
   }
+
 }
