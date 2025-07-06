9bb4e119e25b3e22040bbb7380dcac4e9ae711b2
johnnyw0 <131084912+johnnyw0@users.noreply.github.com>
2025-07-06 00:34:36 -0300

    dockerização de todo o projeto

--- Frontend/src/services/ProductsService.ts
+++ Frontend/src/services/ProductsService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/products';
+const API_URL = import.meta.env.VITE_API_URL as string;
 
 export interface Product {
     id: string
